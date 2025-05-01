a00c9f0007451bb42bf7550e409b970a5a9ccfac
paclem <pascal.moeller@posteo.de>
2025-05-01 21:13:27 +0200

    added content

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from "../background/background.component";
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BackgroundComponent, HeaderComponent],
+  imports: [BackgroundComponent, HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
