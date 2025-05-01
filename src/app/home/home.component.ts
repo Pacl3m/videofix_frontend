c35ffd66f0e3b9eb875d4c06784d136be9849c40
paclem <pascal.moeller@posteo.de>
2025-05-01 15:21:21 +0200

    added header componente

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BackgroundComponent } from "../background/background.component";
+import { HeaderComponent } from "../header/header.component";
 
 @Component({
   selector: 'app-home',
-  imports: [BackgroundComponent],
+  imports: [BackgroundComponent, HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
