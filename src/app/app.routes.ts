672fe0c36c7b3ba993c01c5076a7722d52c370e9
paclem <pascal.moeller@posteo.de>
2025-05-04 15:10:56 +0200

    added validation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { ForgotPWComponent } from './forgot-pw/forgot-pw.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'forgotPW', component: ForgotPWComponent },
+    { path: 'signUp', component: SignUpComponent },
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 import { BackgroundComponent } from "../background/background.component";
 import { HeaderComponent } from "../header/header.component";
 import { FooterComponent } from "../footer/footer.component";
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [BackgroundComponent, HeaderComponent, FooterComponent],
+  imports: [BackgroundComponent, HeaderComponent, FooterComponent, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
